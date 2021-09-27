215236f5f3b519a10e4d32be23032a751c1fe849
Gersey Stelmach <gerseystelmach@gmail.com>
2021-09-27 09:44:37 +0200

    Commiting local changes before pull

---
+++ Workshop/20210922_Classes/src/dojo/Main.java
@@ -0,0 +1,9 @@
+package dojo;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Minha primeira aplicação Java!");
+    }
+}
