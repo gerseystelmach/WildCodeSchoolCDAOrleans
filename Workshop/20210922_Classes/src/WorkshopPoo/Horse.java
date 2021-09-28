a6bd879e0f8fc45fa3c8d63d58a346a5b51508ea
Nicolas Rousset <nrousset@gmail.com>
2021-09-28 08:50:22 +0200

    Feedback for workshop 1

--- Workshop/20210922_Classes/src/WorkshopPoo/Horse.java
+++ Workshop/20210922_Classes/src/WorkshopPoo/Horse.java
@@ -5,6 +5,8 @@ public class Horse extends PawedAnimal {
 
     public Horse(String name, int nbPaws) {
         super(name, nbPaws);
+        // NRO 2021-09-27 : à ne jamais faire ! Transférer les arguments via le 
+        //  constructeur
         this.noise = "iiirrrrí";
         this.howItMoves = "gallop";
     }
