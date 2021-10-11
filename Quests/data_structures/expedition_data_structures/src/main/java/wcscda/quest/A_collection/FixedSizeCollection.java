6706774c32c2ed1b6ddeca2b528913e25f12986b
Nicolas Rousset <nrousset@gmail.com>
2021-10-11 07:22:22 +0200

    Adding quests A and G

---
+++ Quests/data_structures/expedition_data_structures/src/main/java/wcscda/quest/A_collection/FixedSizeCollection.java
@@ -0,0 +1,11 @@
+package wcscda.quest.A_collection;
+
+import wcscda.quest.Sentinel;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+
+class FixedSizeCollection {
+    private Integer[] array;
+}
